d2b30438b1bcbf0805790db58b46a67e40661e6d
Your name <you@example.com>
2021-06-10 16:27:31 +0800

    'hetelmanagement'

---
+++ src/testing/InHotelTest.java
@@ -0,0 +1,19 @@
+package testing;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import hotel.DP;
+import hotel.IData;
+import hotel.InHotel;
+
+public class InHotelTest {
+	protected DP dp=new DP();
+	protected IData id=dp;
+	protected InHotel ih=new InHotel(id);
+	@Test
+	public void testIn() {
+		id.iniRooms();
+		assertEquals("Marry成功入住901房间！",ih.in(901, "Marry"));
+		assertEquals("该房间已经有客人入住！",ih.in(901, "Marry"));
+	}
+
+}
